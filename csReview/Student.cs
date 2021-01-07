9a2461c7af27be06e4391af8ce8735c220e76171
vancodocton <duongtruong.nguyenky@gmail.com>
2021-01-07 15:30:32 +0700

    Add Student class

---
+++ csReview/Student.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csReview
+{
+	class Student
+	{
+		public int Age { get; private set; }
+		public float Gpa { get; private set; }
+		public string Name { get; private set; }
+
+		public Student(int age, float gpa, string name)
+		{
+			Age = age;
+			Gpa = gpa;
+			Name = name;
+		}
+		public void DisplayInformation()
+		{
+			Console.WriteLine($"Student name: \t{Name}");
+			Console.WriteLine($"Student age: \t{Age}");
+			Console.WriteLine($"Student GPA: \t{Gpa}");
+		}
+		public override string ToString()
+		{
+			return base.ToString();
+		}
+	}
+}
