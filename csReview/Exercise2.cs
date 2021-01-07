c9c7595df61a2924ffdddc59c53e510ff87e26eb
vancodocton <duongtruong.nguyenky@gmail.com>
2021-01-07 14:19:08 +0700

    Add Excersie 2 source code

---
+++ csReview/Exercise2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+namespace csReview
+{
+	class Exercise2
+	{
+		public void Run()
+		{
+			int n = InputNum();
+
+			if (CheckPrime(n))
+				Console.WriteLine($"{n} is a prime");
+			else
+				Console.WriteLine($"{n} is not a prime");
+		}
+
+		private int InputNum()
+		{
+			int n;
+
+			do
+			{
+				Console.Write("Enter n bigger than 1: ");
+				n = int.Parse(Console.ReadLine());
+			} while (n < 2);
+
+			return n;
+		}
+
+		private bool CheckPrime(int n)
+		{
+			int sqrtNum = (int)Math.Sqrt(n);
+
+			for (int i = 2; i <= sqrtNum; i++)
+				if (n % i == 0)
+					return false;
+
+			return true;
+		}
+	}
+}
