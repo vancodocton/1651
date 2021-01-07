586d12973750c3ca3fb3015f7f12de51e65b7f73
vancodocton <duongtruong.nguyenky@gmail.com>
2021-01-07 14:08:18 +0700

    Add Exerciser1 code

---
+++ csReview/Exercise1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace csReview
+{
+	class Exercise1
+	{
+		public void Run()
+		{
+			Console.Write("Enter n = ");
+			int n = int.Parse(Console.ReadLine());
+
+			int sum = 0;
+			for (int i = 1; i <= n; i++)
+			{
+				sum += i;
+			}
+
+			Console.WriteLine("Sum of serial number from 1 to {0} is {1}", n, sum);
+		}
+	}
+}
