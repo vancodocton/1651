fc112e2a05368ac2c987ab4c78d56fb8e5b7ad41
vancodocton <duongtruong.nguyenky@gmail.com>
2021-01-07 16:38:46 +0700

    Add source code for object Dice

---
+++ csReview/Dice.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace csReview
+{
+	class Dice
+	{
+		public int Sides { get; private set; }
+		public Dice(int sides = 6)
+		{
+			Sides = sides;
+		}
+		//public Dice()
+		//{
+		//	Sides = 6;
+		//}
+		public int Roll()
+		{
+
+			var random = new Random();
+			return random.Next(1, Sides + 1);
+		}
+	}
+}
