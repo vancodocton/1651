87ac990a46d39625b8f1db25c070d8ce34680119
vancodocton <duongtruong.nguyenky@gmail.com>
2021-01-09 15:05:30 +0700

    relase code

--- PublishSourceCode/Program.cs
+++ PublishSourceCode/Program.cs
@@ -1,181 +1,340 @@
-﻿using System;
-using System.Globalization;
+using System;
+using System.Collections.Generic;
+using System.Numerics;
 
-namespace PublishSourceCode
+namespace Lab00ObjectsAndClasses
 {
-    class Program
-    {
-        static void Main(string[] args)
-        {
-           var app = new reviewCS.Exercise1();
-           app.Run();
-           Console.ReadKey(true);           
-        }
-    }
-}
-
-namespace reviewCS
-{
-    class Exercise1
-    {
-        public void Run()
-        {
-            Console.Write("Enter n = ");
-            int n = int.Parse(Console.ReadLine());
-
-            int sum = 0;
-            for (int i = 1; i <= n; i++)
-            {
-                sum += i;
-            }
-
-            Console.WriteLine("Sum of serial number from 1 to {0} is {1}", n, sum);
-        }
-    }
-    class Exercise2
+	class Program
 	{
-		public void Run()
+		static void Main(string[] args)
 		{
-			int n = InputNum();
+			var app = new VehicleCatalogue();
 
-			if (CheckPrime(n))
-				Console.WriteLine($"{n} is a prime");
-			else
-				Console.WriteLine($"{n} is not a prime");
-		}
-
-		private int InputNum()
-		{
-			int n;
-
-			do
-			{
-				Console.Write("Enter n bigger than 1: ");
-				n = int.Parse(Console.ReadLine());
-			} while (n < 2);
-
-			return n;
-		}
-
-		private bool CheckPrime(int n)
-		{
-			int sqrtNum = (int)Math.Sqrt(n);
-
-			for (int i = 2; i <= sqrtNum; i++)
-				if (n % i == 0)
-					return false;
-
-			return true;
+			app.Run();
+			Console.ReadKey(true);
 		}
 	}
-    class Exercise3
+    class BigFactorial
 	{
-		private int Num;
 		public void Run()
 		{
-			// Generate a random number from 1 to 100
-			Random random = new Random();
-			Num = random.Next(1, 100);
+			int n = int.Parse(Console.ReadLine());
 
-			Console.WriteLine("n is a number in range from 1 to 100");
-			// Guessing n 7 time
-			for (int i = 1; i <= 7; i++)
+			BigInteger factorial = 1;
+
+			for (int i = 2; i <= n; i++)
 			{
-				Console.Write($"Guessing {i} time, n = ");
+				factorial *= i;
+			}
+			Console.WriteLine(factorial);
 
-				// check guessing result
-				int n = int.Parse(Console.ReadLine());
-				if (n == Num)
-				{
-					Console.WriteLine("Congratulations, You're winner!");
-					return;
-				}
-				else
-				{
-					if (n < Num)
-						Console.WriteLine("Opps, You're wrong! Guessed number is smaller than n");
-					else
-						Console.WriteLine("Opps, You're wrong! Guessed number is bigger than n");
-				}
+		}
+	}
+    class Song
+	{
+		public string TypeList { get; set; }
+		public string Name { get; set; }
+		public string Time { get; set; }
+	}
+	class SongList
+	{
+		public int Number { get; set; }
+		public List<Song> Songs { get; set; }
+		public SongList()
+		{
+			Songs = new List<Song>();
+			Number = 0;
+		}
+			
+		public void Run()
+		{
+			int Number = int.Parse(Console.ReadLine());
+
+			for (int i = 0; i < Number; i++)
+			{
+				string[] raw = Console.ReadLine().Split('_');
+				AddSong(raw);
 			}
 
-			// Print n if guessing fail
-			Console.WriteLine("Sorry, You're fail!");
-			Console.WriteLine($"n = {Num}");
-			return;
+			string typeList = Console.ReadLine();
+			DisplaySongListType(typeList);
+		}
+		public void AddSong(string[] raw)
+		{
+			string type = raw[0];
+			string name = raw[1];
+			string time = raw[2];
+
+			Song song = new Song
+			{
+				TypeList = type,
+				Name = name,
+				Time = time
+			};
+			Songs.Add(song);
+		}
+		public void DisplaySongListType(string typeList)
+		{
+			if (typeList == "all")
+			{
+				foreach (var song in Songs)
+				{
+					Console.WriteLine(song.Name);
+				}
+			}
+			else
+			{
+				foreach (var song in Songs)
+				{
+					if (song.TypeList == typeList)
+						Console.WriteLine(song.Name);
+				}
+			}
 		}
 	}
     class Student
 	{
-		private int age;
-		private double gpa;
-		private string name;
-		public int Age { get => age; private set => age = value; }
-		public double Gpa { get => gpa; private set => gpa = value; }
-		public string Name { get => name; private set => name = value; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int Age { get; set; }
+		public string HomeTown { get; set; }
+	}
 
-		public Student(string name, int age, double gpa)
+	class StudentList
+	{
+		public List<Student> Students { get; set; }
+
+		public StudentList()
 		{
-			Age = age;
-			Gpa = gpa;
-			Name = name;
+			Students = new List<Student>();
 		}
-		public void DisplayInformation()
+		public void Run()
 		{
-			Console.WriteLine($"Student name: \t{name}");
-			Console.WriteLine($"Student age: \t{age}");
-			Console.WriteLine($"Student GPA: \t{gpa}");
+			string[] raw = Console.ReadLine().Split();
+
+			do
+			{
+				Add(raw);
+				raw = Console.ReadLine().Split();
+			}
+			while (raw[0] != "end");
+
+
+			string city = Console.ReadLine();
+			DisplayStudentInfoFromCity(city);
+		}
+		public void Add(string[] raw)
+		{
+			Student student = new Student
+			{
+				FirstName = raw[0],
+				LastName = raw[1],
+				Age = int.Parse(raw[2]),
+				HomeTown = raw[3]
+			};
+			Students.Add(student);
+		}
+		public void DisplayStudentInfoFromCity(string city)
+		{
+			foreach (var student in Students)
+			{
+				if (student.HomeTown == city)
+				{
+					Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+				}
+			}
 		}
 	}
-    
-}
+    class StudentList2
+	{
+		public List<Student> Students { get; set; }
 
-namespace Lab_ObjectsAndClasses
-{
-	class DayOfWeek
+		public StudentList2()
+		{
+			Students = new List<Student>();
+		}
+		public void Run()
+		{
+			string[] raw = Console.ReadLine().Split();
+
+			do
+			{
+				Add(raw);
+				raw = Console.ReadLine().Split();
+			}
+			while (raw[0] != "end");
+
+
+			string city = Console.ReadLine();
+			DisplayStudentInfoFromCity(city);
+		}
+		public void Add(string[] raw)
+		{
+			Student student = new Student
+			{
+				FirstName = raw[0],
+				LastName = raw[1],
+				Age = int.Parse(raw[2]),
+				HomeTown = raw[3]
+			};
+
+			for (int i = 0; i < Students.Count; i++)
+			{
+				if (student.FirstName == Students[i].FirstName && student.LastName == Students[i].LastName)
+				{
+					Students[i] = student;
+					return;
+				}
+			}
+			Students.Add(student);
+		}
+		public void DisplayStudentInfoFromCity(string city)
+		{
+			foreach (var student in Students)
+			{
+				if (student.HomeTown == city)
+				{
+					Console.WriteLine($"{student.FirstName} {student.LastName} is {student.Age} years old.");
+				}
+			}
+		}
+    }
+    class Item
+	{
+		public string Name;
+		public double Price;
+	}
+	class Box
+	{
+		public Box()
+		{
+			Item = new Item();
+		}
+		public int SerialNumber { get; set; }
+		public Item Item { get; set; }
+		public int ItemQuality { get; set; }
+		public double Price
+		{
+			get
+			{
+				return Item.Price * ItemQuality;
+			}
+		}
+	}
+	class StoreBoxs
+	{
+		public List<Box> Boxs { get; set; }
+		public StoreBoxs()
+		{
+			Boxs = new List<Box>();
+		}
+		public void Run()
+		{
+			string line = Console.ReadLine();
+
+			while (line != "end")
+			{
+				string[] raw = line.Split();
+				Box box = new Box();
+				box.SerialNumber = int.Parse(raw[0]);
+				box.Item.Name = raw[1];
+				box.ItemQuality = int.Parse(raw[2]);
+				box.Item.Price = double.Parse(raw[3]);
+				Boxs.Add(box);
+				line = Console.ReadLine();
+			}
+
+			Boxs.Sort(Comparision);
+			foreach (var box in Boxs)
+			{
+				Console.WriteLine(box.SerialNumber);
+				Console.WriteLine("-- {0} - ${1:F2}: {2}", box.Item.Name, box.Item.Price, box.ItemQuality);
+				Console.WriteLine("-- ${0:F2}", box.Price);
+			}
+		}
+		private static int Comparision(Box box1, Box box2)
+		{
+			return box2.Price.CompareTo(box1.Price);
+		}
+	}
+    	class Car
+	{
+		public string Brand { get; set; }
+		public string Model { get; set; }
+		public double HorsePower { get; set; }
+	}
+	class Truck
+	{
+		public string Brand { get; set; }
+		public string Model { get; set; }
+		public double Weight { get; set; }
+	}
+	class Catalog
+	{
+		public List<Car> Cars { get; set; }
+		public List<Truck> Trucks { get; set; }
+		public Catalog()
+		{
+			Cars = new List<Car>();
+			Trucks = new List<Truck>();
+		}
+	}
+	class VehicleCatalogue
 	{
 		public void Run()
 		{
-			// Enter date time
-			var dateString = Console.ReadLine();
+            Catalog catalog = new Catalog();
 
-			// convert raw date time to the suitable format
-			CultureInfo provider = CultureInfo.CurrentCulture;
-			var datetime = DateTime.ParseExact(dateString, "dd-MM-yyyy", provider);
+            string line = Console.ReadLine();
 
-			// Print the resultss
-			Console.WriteLine(datetime.DayOfWeek);
-		}
-	}
-	class RandomizeWords
-	{
-		public void Run()
-		{
-			// Enter the sentences
-			var sentence = Console.ReadLine();
+            while (line != "end")
+            {
+                string[] raw = line.Split('/');
 
-			// Split words in the entered sentence
-			var words = sentence.Split(null as char[], StringSplitOptions.RemoveEmptyEntries);
+                if (raw[0] == "Car")
+                {
+                    Car car = new Car
+                    {
+                        Brand = raw[1],
+                        Model = raw[2],
+                        HorsePower = double.Parse(raw[3])
+                    };
+                    catalog.Cars.Add(car);
+                }
+                else if (raw[0] == "Truck")
+                {
+                    Truck truck = new Truck()
+                    {
+                        Brand = raw[1],
+                        Model = raw[2],
+                        Weight = double.Parse(raw[3])
+                    };
+                    catalog.Trucks.Add(truck);
+                }
+                line = Console.ReadLine();
+            }
 
-			// Random the order of words in sentences
-			var random = new Random();
+            catalog.Cars.Sort(CmpCar);
+            Console.WriteLine("Cars:");
+            foreach (var car in catalog.Cars)
+            {
+                Console.WriteLine($"{car.Brand}: {car.Model} - {car.HorsePower}hp");
+            }
+            catalog.Trucks.Sort(CmpTruck);
+            Console.WriteLine("Trucks");
+            foreach (var truck in catalog.Trucks)
+            {
+                Console.WriteLine($"{truck.Brand}: {truck.Model} - {truck.Weight}kg");
+            }
 
-			for (int i = 0; i < words.Length; i++)
-			{
-				Swap(ref words[i], ref words[random.Next(0, words.Length)]);
-			}
-
-			// Print output
-			foreach (var word in words)
-			{
-				Console.WriteLine(word);
-			}
-		}
-		private void Swap(ref string s1, ref string s2)
-		{
-			string tmp = s1;
-			s1 = s2;
-			s2 = tmp;
-		}
-	}
+        }
+        private static int CmpCar(Car car1, Car car2)
+        {
+            return car1.Brand.CompareTo(car2.Brand);
+        }
+        private static int CmpTruck(Truck truck1, Truck truck2)
+        {
+            return truck1.Brand.CompareTo(truck2.Brand);
+        }
+    }
 }
