9e41664f9f77d0550416d6312e5a87c4167eba5c
vancodocton <duongtruong.nguyenky@gmail.com>
2021-01-09 16:23:56 +0700

    Add classes Franction abd MathFranction

---
+++ L2ClassMethod/Fraction.cs
@@ -0,0 +1,53 @@
+﻿using System;
+
+namespace L2ClassMethod
+{
+	class Fraction
+	{
+		public int Numerator { get; private set; }
+		public int Denominator { get; private set; }
+		public Fraction(int numerator, int denominator)
+		{
+			if (denominator == 0)
+				throw new DivideByZeroException();
+
+			int gcd = GCD(numerator, denominator);
+			Numerator = numerator / gcd;
+			Denominator = denominator / gcd;
+
+			if (Denominator < 0)
+			{
+				Denominator *= -1;
+				Numerator *= -1;
+			}
+		}
+		protected static int GCD(int a, int b)
+		{
+			if (Math.Abs(a) < Math.Abs(b))
+				Swap(ref a, ref b);
+
+			int tmp;
+			while (b != 0)
+			{
+				tmp = b;
+				b = a % b;
+				a = tmp;
+			}
+			return a;
+		}
+		protected static void Swap(ref int a, ref int b)
+		{
+			int tmp = a;
+			a = b;
+			b = tmp;
+		}
+		public override string ToString()
+		{
+			return $"{Numerator} / {Denominator}\n";
+		}
+		public decimal ToDecimal()
+		{
+			return (decimal)Numerator / Denominator;
+		}
+	}
+}
---
+++ L2ClassMethod/MathFranction.cs
@@ -0,0 +1,36 @@
+﻿namespace L2ClassMethod
+{
+	class MathFranction
+	{
+		public static Fraction Add(Fraction a, Fraction b)
+		{
+			int numerator = a.Numerator * b.Denominator + b.Numerator * a.Denominator;
+			int denominator = a.Denominator * b.Denominator;
+
+			return new Fraction(numerator, denominator);
+		}
+
+		public static Fraction Subtract(Fraction a, Fraction b)
+		{
+			int numerator = a.Numerator * b.Denominator - b.Numerator * a.Denominator;
+			int denominator = a.Denominator * b.Denominator;
+
+			return new Fraction(numerator, denominator);
+		}
+
+		public static Fraction Multiply(Fraction a, Fraction b)
+		{
+			int numerator = a.Numerator * b.Numerator;
+			int denominator = a.Denominator * b.Denominator;
+
+			return new Fraction(numerator, denominator);
+		}
+		public static Fraction Divide(Fraction a, Fraction b)
+		{
+			int numerator = a.Numerator * b.Denominator;
+			int denominator = a.Denominator * b.Numerator;
+
+			return new Fraction(numerator, denominator);
+		}
+	}
+}
