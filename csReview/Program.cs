d46c36b44aaf7d2b074b38a2105bcf8e54833fcd
vancodocton <duongtruong.nguyenky@gmail.com>
2021-01-09 13:32:41 +0700

    Update project file

--- csReview/Program.cs
+++ csReview/Program.cs
@@ -6,11 +6,16 @@ namespace csReview
 		static void Main(string[] args)
 		{
 			//To run code of other exercise, change Exercise3 to the class representing for it
-			var app = new Exercise3();
-			app.Run();
 
-			Console.ReadKey(true);
 
+			//do
+			//{
+			//	Console.WriteLine(dice.Roll());
+			//	//var key = Console.ReadKey(true).Key.ToString();
+			//} while (Console.ReadKey(true).Key.ToString() == "Enter");
+
+			var student = new Student("Nguyen Ky Duong Truong", 19, 8.9);
+			student.DisplayInformation();
 		}
 	}
 }
