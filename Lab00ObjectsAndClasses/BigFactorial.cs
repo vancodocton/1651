f32e709fe55c2ab23f37a1ba43507baa52380c34
vancodocton <duongtruong.nguyenky@gmail.com>
2021-01-09 13:29:38 +0700

    Add Exercise 3

---
+++ Lab00ObjectsAndClasses/BigFactorial.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Numerics;
+
+namespace Lab00ObjectsAndClasses
+{
+	class BigFactorial
+	{
+		public void Run()
+		{
+			int n = int.Parse(Console.ReadLine());
+
+			BigInteger factorial = 1;
+
+			for (int i = 2; i <= n; i++)
+			{
+				factorial *= i;
+			}
+			Console.WriteLine(factorial);
+
+		}
+	}
+}
