c54b9725d9e57ccf4a50bb29d36d3ff29d04abf1
vancodocton <duongtruong.nguyenky@gmail.com>
2021-01-21 16:51:28 +0700

    Update

---
+++ W2.1.Inheritance/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace W2._1.Inheritance
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			RandomList randomList = new RandomList();
+
+			for (int i = 0; i < 10; i++)
+			{
+				randomList.Add(i.ToString());
+			}
+
+			Console.WriteLine(randomList.RemoveRandomElement());
+			Console.WriteLine(randomList.RemoveRandomElement());
+
+			Console.ReadKey(true);
+		}
+	}
+}
---
+++ W2.1.Inheritance/RandomList.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace W2._1.Inheritance
+{
+	public class RandomList : List<string>
+	{
+		private readonly Random random = new Random();
+		public string RemoveRandomElement()
+		{
+			int index = random.Next(0, this.Count);
+			string str = this[index];
+			this.RemoveAt(index);
+			return str;
+		}
+	}
+}
