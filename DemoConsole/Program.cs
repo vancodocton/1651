ba6e35b65d4a632982f54c2973472b2707a72311
vancodocton <duongtruong.nguyenky@gmail.com>
2021-01-21 16:49:43 +0700

    Add DemoConsole Project

---
+++ DemoConsole/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DemoConsole
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			var students = new List<Student>();
+
+			students.Add(new Student("Truong", 19));
+			students.Add(new Student("Thien", 18));
+			students.Add(new Student("Lam", 20));
+
+			foreach (var student in students)
+				Console.WriteLine(student);
+			students.Sort(new Student.SortNameAscending());
+
+			foreach (var student in students)
+				Console.WriteLine(student);
+
+			Console.ReadKey(true);
+		}
+
+	}
+}
