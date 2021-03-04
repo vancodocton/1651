434d10174b5e664c5f4583dc4a57a38c29e11997
vancodocton <duongtruong.nguyenky@gmail.com>
2021-03-04 13:07:51 +0700

    refactory interface to abstract class

---
+++ 1651.Assignment2/MilkTea.cs
@@ -0,0 +1,9 @@
+﻿namespace _1651.Assignment2
+{
+	abstract class MilkTea
+	{
+		public abstract double GetCost();
+		public abstract string GetDescription();
+		public override string ToString() => $"{GetDescription()}; Cost: {GetCost()}";
+	}
+}
